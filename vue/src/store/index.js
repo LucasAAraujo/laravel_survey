8bb8cfad28e363a56d52c8ec6602da1cdedf0683
Lucas Alves <lucasalves3.0@hotmail.com>
2024-10-12 17:37:57 -0300

    update: projeto criado, vue e componentes insladaos.

---
+++ vue/src/store/index.js
@@ -0,0 +1,11 @@
+const { createStore } = require("vuex/types/index.js");
+
+const store = createStore({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {},
+    modules: {}
+})
+
+export default store;
